a8b3cb033e95ca594dd2e7bb444dd61eb5146dd0
Valera Kleban <petrpetrovich106@gmail.com>
2023-11-12 15:42:44 +0300

    add my assembly gulp

---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+// import { Swiper } from './swiper/swiper-js';
+
+
+// headerSlider
+// function headerSliderInit() {
+//    const swiperContent = new Swiper('.header-slider', {
+//       spaceBetween: 30,
+//       effect: 'fade',
+//       speed: 1500,
+//       fadeEffect: {
+//          crossFade: true
+//       },
+
+//       autoplay: {
+//          delay: 2000,
+//          disableOnInteraction: false,
+//       },
+//       pagination: {
+//          el: ".swiper-pagination",
+//          clickable: true,
+//       },
+//    })
+
+// }
+// headerSliderInit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const headerBtnDecor = document.querySelectorAll('.header__top-btn--decor');
+// const headerTopList = document.querySelectorAll(".header__top-list");
+
+// headerBtnDecor.forEach(function (element) {
+//    element.addEventListener('click', disable)
+// })
+
+// function disable(evt) {
+//    headerTopList.forEach(function (item) {
+//       item.classList.toggle('header__top-list--active');
+//       console.log(item.classList.contains("header__top-list--active"))
+
+//    });
+// }
+
+
+
+
+
+
